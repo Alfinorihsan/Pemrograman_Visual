6ecfc78c811e4cd702d5a64864588761b1b8ad14
Alfi Nor Ihsan <90581008+Alfinorihsan@users.noreply.github.com>
2023-04-06 19:11:09 +0800

    Add files via upload

---
+++ Posttest 4/Posttest 4/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnKeluar = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtHarga = New System.Windows.Forms.TextBox()
+        Me.cmbUkuran = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(66, 244)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'txtId
+        '
+        Me.txtId.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtId.Location = New System.Drawing.Point(179, 241)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(324, 30)
+        Me.txtId.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(60, 520)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(602, 222)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTambah.Location = New System.Drawing.Point(60, 450)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(123, 42)
+        Me.btnTambah.TabIndex = 3
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Posttest_4.My.Resources.Resources._20230402_183515__1_
+        Me.PictureBox1.Location = New System.Drawing.Point(150, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(381, 177)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'btnKeluar
+        '
+        Me.btnKeluar.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnKeluar.Location = New System.Drawing.Point(292, 450)
+        Me.btnKeluar.Name = "btnKeluar"
+        Me.btnKeluar.Size = New System.Drawing.Size(123, 42)
+        Me.btnKeluar.TabIndex = 6
+        Me.btnKeluar.Text = "Keluar"
+        Me.btnKeluar.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHapus.Location = New System.Drawing.Point(539, 450)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(123, 42)
+        Me.btnHapus.TabIndex = 7
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(539, 305)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(123, 42)
+        Me.btnReset.TabIndex = 8
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(179, 285)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(324, 30)
+        Me.txtNama.TabIndex = 9
+        '
+        'txtHarga
+        '
+        Me.txtHarga.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHarga.Location = New System.Drawing.Point(179, 377)
+        Me.txtHarga.Name = "txtHarga"
+        Me.txtHarga.Size = New System.Drawing.Size(324, 30)
+        Me.txtHarga.TabIndex = 10
+        '
+        'cmbUkuran
+        '
+        Me.cmbUkuran.FormattingEnabled = True
+        Me.cmbUkuran.Items.AddRange(New Object() {"XS", "S", "M", "L", "XL", "XXL"})
+        Me.cmbUkuran.Location = New System.Drawing.Point(179, 332)
+        Me.cmbUkuran.Name = "cmbUkuran"
+        Me.cmbUkuran.Size = New System.Drawing.Size(324, 28)
+        Me.cmbUkuran.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(66, 290)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 25)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(66, 335)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 25)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Ukuran"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(66, 380)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 25)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Harga"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Franklin Gothic Medium Cond", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(175, 159)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(337, 30)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Manajemen Data Kostum Anna-Chan"
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Pink
+        Me.ClientSize = New System.Drawing.Size(718, 831)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmbUkuran)
+        Me.Controls.Add(Me.txtHarga)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnKeluar)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnKeluar As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtHarga As TextBox
+    Friend WithEvents cmbUkuran As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Posttest 4/Posttest 4/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+Public Class form1
+    Sub Reset()
+        txtId.Clear()
+        txtNama.Clear()
+        cmbUkuran.Focus()
+        cmbUkuran.Text = ""
+        txtHarga.Clear()
+        txtId.Focus()
+    End Sub
+    Sub Isi()
+        txtNama.Clear()
+        txtNama.Focus()
+        cmbUkuran.Focus()
+        txtHarga.Clear()
+        txtHarga.Focus()
+    End Sub
+    Sub TampilJenis()
+        DA = New MySqlDataAdapter("Select * From tbkostum", CONN)
+        DS = New DataSet
+        DS.Clear()
+        DA.Fill(DS, "tbkostum")
+        DataGridView1.DataSource = DS.Tables("tbkostum")
+        DataGridView1.Refresh()
+    End Sub
+
+    Private Sub DataJenis_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call koneksi()
+        Call TampilJenis()
+        Call Reset()
+    End Sub
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        If txtId.Text = "" Or txtNama.Text = "" Or cmbUkuran.Text = "" Or txtHarga.Text = "" Then
+            MsgBox("Mohon lengkapi data terlebih dahulu, Admin-San")
+            txtId.Focus()
+            Exit Sub
+        Else
+            CMD = New MySqlCommand("Select * From tbkostum where id='" & txtId.Text & "'", CONN)
+            RD = CMD.ExecuteReader
+            RD.Read()
+            RD.Close()
+            If Not RD.HasRows Then
+                Dim Simpan As String = "insert into tbkostum (ID, Nama, Ukuran, Harga) values ('" & txtId.Text & "','" & txtNama.Text & "','" & cmbUkuran.Text & "','" & txtHarga.Text & "')"
+                CMD = New MySqlCommand(Simpan, CONN)
+                CMD.ExecuteNonQuery()
+                MsgBox("Data berhasil di simpan, Admin-San!", MsgBoxStyle.Information, "HOREE")
+            End If
+            Call TampilJenis()
+            Call Reset()
+            txtId.Focus()
+        End If
+    End Sub
+
+    Private Sub btnKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        Dim result As DialogResult = MessageBox.Show("Apakah anda yakin untuk keluar, Admin-San?", "Konfirmasi", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            End
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Call Reset()
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If txtId.Text = "" Then
+            MsgBox("ID belum diisi, Admin-San")
+            txtId.Focus()
+        Else
+            If MessageBox.Show("Apakah anda yakin untuk menghapus data " & txtId.Text & ", Admin-San?", "", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+                CMD = New MySqlCommand("Delete From tbkostum Where id = '" & txtId.Text & "'", CONN)
+                CMD.ExecuteNonQuery()
+                Call Reset()
+                Call TampilJenis()
+            Else
+                Call Reset()
+            End If
+        End If
+
+    End Sub
+End Class
+
+
